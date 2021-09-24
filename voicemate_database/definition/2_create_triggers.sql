07fb1c45c2ab90a5d1bfe479674c276c4c3fb39d
hatano-viviane <rikuhatano09@gmail.com>
2021-09-24 20:30:50 +0900

    feat: Add trigger in DB.

---
+++ voicemate_database/definition/2_create_triggers.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION set_update_time()
+RETURNS trigger AS $$
+    BEGIN
+      NEW.updated_at = NOW();
+      RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- Users
+DROP TRIGGER IF EXISTS "users_updated_at_trigger" ON "users";
+CREATE TRIGGER "users_updated_at_trigger"
+BEFORE UPDATE ON "users"
+FOR EACH ROW
+EXECUTE PROCEDURE set_update_time();
