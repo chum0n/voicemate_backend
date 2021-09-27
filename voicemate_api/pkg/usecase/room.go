411051c8f3e8a388ee432536c9e26ffb92909e7e
chum0n <cyumon_tkt.0914@icloud.com>
2021-09-27 15:18:15 +0900

    add usecase

---
+++ voicemate_api/pkg/usecase/room.go
@@ -0,0 +1,18 @@
+package usecase
+
+import (
+	"github.com/rakutenshortintern2021-D-utopia/D-4_2/internal/infrastructure/persistence"
+	"github.com/rakutenshortintern2021-D-utopia/D-4_2/pkg/domain/model"
+)
+
+// GetRoom gets room matched with condition.
+func GetRoom(id uint64) model.Room {
+	roomPersistence := persistence.NewRoomPersistence()
+
+	room, err := roomPersistence.FindRoomByID(id)
+	if err != nil {
+		panic(err)
+	}
+
+	return room
+}
