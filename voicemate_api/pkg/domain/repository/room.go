2195150983c0e72eeb425c5e08e3a2595b8732a6
chum0n <cyumon_tkt.0914@icloud.com>
2021-09-27 23:59:46 +0900

    add repo and persistence

--- voicemate_api/pkg/domain/repository/room.go
+++ voicemate_api/pkg/domain/repository/room.go
@@ -9,5 +9,7 @@ type (
 	RoomRepository interface {
 		FindRoomByID(id uint64) (model.Room, error)
 		GetRooms(name string, age_lower uint32, age_upper uint32, gender string, member_limit uint32) ([]model.Room, error)
+		UpdateRoom(uint64, map[string]interface{}) (model.Room, error)
+		SaveTags(uint64, []uint64) error
 	}
 )
