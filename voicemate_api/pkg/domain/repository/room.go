25e3aeea0dd0a31800dd23bbd0328af5599d26c6
chum0n <cyumon_tkt.0914@icloud.com>
2021-09-27 10:49:02 +0900

    add repository

---
+++ voicemate_api/pkg/domain/repository/room.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"github.com/rakutenshortintern2021-D-utopia/D-4_2/pkg/domain/model"
+)
+
+type (
+	// RoomRepository is the interface of Room repository.
+	RoomRepository interface {
+		FindRoom(id uint64) (model.Room, error)
+		// GetAll() ([]model.Room, error)
+	}
+)
